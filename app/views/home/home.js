b70d91b2588a9e4db4fef3bd169faf48f6e7fdb6
sudeep <sudeep@mobikontech.com>
2015-10-09 15:00:57 +0530

    removed unused codes

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -8,20 +8,4 @@ module.exports = {
 /*@ngInject*/
 function HomeController($scope, $state) {
     $scope.stateName = $state.current.name;
-
-	$scope.myInterval = 5000;
-	$scope.noWrapSlides = false;
-	
-	var slides = $scope.slides = [];
-	$scope.addSlide = function() {
-	var newWidth = 600 + slides.length + 1;
-	slides.push({
-	  image: '//placekitten.com/' + newWidth + '/300',
-	  text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-	    ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-	});
-	};
-	for (var i=0; i<4; i++) {
-		$scope.addSlide();
-	}
 }
